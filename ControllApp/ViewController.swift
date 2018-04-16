71a5705f5a774512f483a4e34bfa501495d09909
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-04-16 15:26:08 +0200

    Initial Commit

---
+++ ControllApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ControllApp
+//
+//  Created by Johan Hjalmarsson on 2018-04-16.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
