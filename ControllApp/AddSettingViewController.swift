9d3cf5b5d6d44f1d8f4943871552d08cbb9db053
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-04-23 10:50:19 +0200

    add settings func

--- ControllApp/AddSettingViewController.swift
+++ ControllApp/AddSettingViewController.swift
@@ -8,20 +8,60 @@
 
 import UIKit
 
-class AddSettingViewController: UIViewController {
+class AddSettingViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var heightInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        heightInput.delegate = self
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    func addSettingToCoreData() {
+        if let name = nameInput.text, let height = heightInput.text {
+            if !name.isEmpty && !height.isEmpty {
+                if let intHeight = Int16(height) {
+                    CoreDataHandler.saveSetting(name: name, height: intHeight)
+                } else {
+                    print("Failed to convert String to Int16")
+                }
+            } else {
+                print("Name or Height is empty")
+            }
+        } else {
+            print("Failed to unwrap nameInput and/or heightInput")
+        }
     }
     
-
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var result = true
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        result = allowedCharacters.isSuperset(of: characterSet)
+        
+        if let text = heightInput.text {
+            if text.count > 2 {
+                if string != "" {
+                   result = false
+                }
+            }
+        }
+        return result
+    }
+    
+    @IBAction func addButtonClicked(_ sender: Any) {
+        addSettingToCoreData()
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
