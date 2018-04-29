4a8582b02e2ec96e10c309de5f2b30a6728cabb1
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-04-29 14:43:04 +0200

    popViewController

--- ControllApp/AddSettingViewController.swift
+++ ControllApp/AddSettingViewController.swift
@@ -57,18 +57,8 @@ class AddSettingViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func addButtonClicked(_ sender: Any) {
         addSettingToCoreData()
+        self.navigationController?.popViewController(animated: true)
     }
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
